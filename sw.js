16a51778b93ac372e84ef494eba69a655852c637
marecohernan-byte <marecohernan@hotmail.com>
2026-06-12 15:39:55 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portravel-reels-v8-4';
+const CACHE = 'portravel-reels-v8-6';
 const APP = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
